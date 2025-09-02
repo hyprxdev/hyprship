5e7fa9060b61ef1bb259eb2909f84952f579e929
hyprxdev <180069990+hyprxdev@users.noreply.github.com>
2025-09-02 15:39:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# hyprship
+
+Series of tools for building, deploying, and hosting
+applications.
---
+++ cmd/hs/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/hyprxdev/hyprship/cmd/hs/cmd"
+
+func main() {
+	cmd.Execute()
+}
